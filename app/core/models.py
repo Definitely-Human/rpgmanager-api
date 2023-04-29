8eb6ede3ad436e77a3d5af04407a8edfab34fa7f
DefinitelyHuman <odest66@gmail.com>
2023-04-29 15:38:02 +0200

    First registration test and custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,21 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import{
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+}
+from django.utils import timezone
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
