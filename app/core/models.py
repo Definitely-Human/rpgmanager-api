e0a38a4df133f9f34eff4d140eaa183682c6c81d
DefinitelyHuman <odest66@gmail.com>
2023-04-29 16:05:36 +0200

    Changed default user model in settings. Added UserManager. Migration

--- app/core/models.py
+++ app/core/models.py
@@ -3,12 +3,25 @@ Database models.
 """
 
 from django.db import models
-from django.contrib.auth.models import{
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
-}
+)
 from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
@@ -17,5 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
