a18584ce2babafc2de976aba8e904897aa2643ee
DefinitelyHuman <odest66@gmail.com>
2023-04-29 17:49:11 +0200

    settings fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +81,7 @@ DATABASES = {
         'HOST': os.environ.get("DB_HOST"),
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASS")
+        'PASSWORD': os.environ.get("DB_PASS"),
     }
 }
 
