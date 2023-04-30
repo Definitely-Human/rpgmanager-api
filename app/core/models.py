78035d910b37d19d274f3d9b337f9c0aa8c2d309
DefinitelyHuman <34219093+Definitely-Human@users.noreply.github.com>
2023-04-30 16:45:43 +0200

    Feature/custom user model (#2)
    * First registration test and custom user model
    
    * Changed default user model in settings. Added UserManager. Migration
    
    * Email normalization and tests.
    
    * User creation validation and tests.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,50 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, username, password=None, **extra_fields):
+        """Create, save and return a new user. """
+        if not email:
+            raise ValueError('User mush have an email address.')
+        if not username:
+            raise ValueError('User mush have an username.')
+        email = self.normalize_email(email)
+        user = self.model(email=email,username = username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        """Create and return a new superuser."""
+        user = self.create_user(email,username,password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,56 @@
+"""
+Tests for models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        username = 'test'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            username=username,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@Example.COM', 'TEST3@example.com'],
+            ['test4@Example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            username = expected[:5]
+            user = get_user_model().objects.create_user(email, username, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a Value Error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','testusername','test123')
+
+    def test_new_user_without_username_raises_error(self):
+        """Test that creating a user without an username raises a Value Error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('test@example.com','','test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'testusername',
+            'test123',
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
