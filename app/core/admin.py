c3a1ac30e66210460047510a3f88fee139f7758f
DefinitelyHuman <odest66@gmail.com>
2023-04-30 17:56:44 +0200

    User edit page in admin and tests.

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -10,5 +11,20 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'username']
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_("Important dates"), {'fields':('last_login','date_joined')}),
+    )
+    readonly_fields = ['last_login', 'date_joined']
 
 admin.site.register(models.User, UserAdmin)
