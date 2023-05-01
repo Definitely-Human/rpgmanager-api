8cc75f26c770b1e830a4df53281b5b3b5e390454
DefinitelyHuman <odest66@gmail.com>
2023-05-01 16:07:59 +0200

    Added user registration. Login token tests.

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third party packages
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
     # RPG Manager apps
     'core',
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,8 +8,11 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
+import logging
+logger = logging.getLogger('my_logger')
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -46,20 +49,73 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         payload['username'] = 'TestName1'
         res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_with_username_exists_error(self):
+        """Test error returned if user with email exists."""
+        payload = {
+            'email': 'test0@example.com',
+            'password': 'testpass123',
+            'username': 'TestName',
+        }
+        create_user(**payload)
+        payload['email'] = 'test1@example.com'
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars."""
+        MIN_CHARS_IN_PASS = 5
         payload = {
             'email': 'test@example.com',
-            'password': 'pw',
-            'name': 'Test name',
+            'password': '',
+            'name': 'Testname',
         }
-        res = self.client.post(CREATE_USER_URL, payload)
+        for i in range(MIN_CHARS_IN_PASS):
+            payload['password'] = "a" * i
 
+            res = self.client.post(CREATE_USER_URL, payload)
+            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+            user_exists = get_user_model().objects.filter(
+                email=payload['email']
+            ).exists()
+            self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test generates token for valid credentials."""
+        user_details = {
+            'username': 'Testname',
+            'email': 'test@example.com',
+            'password': 'test-user-password123',
+        }
+        create_user(**user_details)
+
+        payload = {
+            'email': user_details['email'],
+            'password': user_details['password'],
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bed_credentials(self):
+        """Test returns error if credentials invalid."""
+        create_user(email='test@example.com',
+                    password='goodpass',
+                    username='Testname',)
+
+        payload = {'email': 'test@example.com','password':'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(
-            email=payload['email']
-        ).exists()
-        self.assertFalse(user_exists)
+
+    def test_create_token_blank_password(self):
+        """Test posting a blank password returns an error."""
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
