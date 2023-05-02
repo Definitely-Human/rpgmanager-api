9cd45da541d8909f2c288654838ac4302e179e20
DefinitelyHuman <odest66@gmail.com>
2023-05-02 17:45:52 +0200

    Added prifile model and profile created on registration test.

--- app/djangoapps/social/models.py
+++ app/djangoapps/social/models.py
@@ -1,3 +1,20 @@
+"""
+Models for social app.
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Profile model."""
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    is_online = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    about_me = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
