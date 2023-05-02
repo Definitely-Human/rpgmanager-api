b3b36b092b198cc8034fe4e50dfdbd730327dafb
DefinitelyHuman <34219093+Definitely-Human@users.noreply.github.com>
2023-05-02 14:35:12 +0300

    Feature/user app (#5)
    * Added user app
    
    * Create user tests.
    
    * Added user registration. 
    
    * Token authentication and tests. Tests for me/ endpoint
    
    * Implemented me/ endpoint.
    
    * drf-spectacular update.
    
    * Linting.

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    # Third party packages
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
+    # RPG Manager apps
+    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -30,6 +30,7 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls')),
 
 ]
