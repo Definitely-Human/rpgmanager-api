2da00572b8d29c5eb689c1ca2f204dd463151604
DefinitelyHuman <odest66@gmail.com>
2023-05-07 23:10:55 +0200

    Improved character model. Changed profile view to GenericViewSet.

--- app/djangoapps/rpg/models.py
+++ app/djangoapps/rpg/models.py
@@ -14,16 +14,13 @@ class Character(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    character_xp = models.IntegerField(
-        validators=[
-            MinValueValidator
-        ],  # TODO change to PositiveBigIntegerField
+    character_xp = models.PositiveIntegerField(
         default=0,
     )
     coins = models.IntegerField(default=0)
     created_at = models.DateTimeField(
-        default=timezone.now
-    )  # TODO change default to auto_now_add
+        auto_now_add=True,
+    )
 
     def __str__(self) -> str:
         return self.name
