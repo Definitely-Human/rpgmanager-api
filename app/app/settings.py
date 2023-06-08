1859d9b907df0f379c46540b386a06bcce440000
DefinitelyHuman <34219093+Definitely-Human@users.noreply.github.com>
2023-06-08 09:32:16 +0000

    Feature/deploy (#14)
    * Added uwsgi.
    
    * Added nginx configuration.
    
    * Added docker-compose-deploy and env sample.
    
    * Added env.sample and enabled settings.py to use them

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,14 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-too8q4+-3$f8k4y9cbft_spxl+$njqs)fh2rd8*d1r^lh$(x^-"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(None, os.environ.get("ALLOWED_HOSTS", "").split(","))
+)
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
